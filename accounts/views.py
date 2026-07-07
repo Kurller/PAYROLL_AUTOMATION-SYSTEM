a5cb19c132854bfeff1d9d72aa3fd65076df89c4
Kurller <kolaquadry@gmail.com>
2026-07-07 23:24:47 +0100

    first commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,27 +1,10 @@
 from django.http import JsonResponse
+from django.conf import settings
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
 
 def test_auth(request):
-    try:
-        username = "ade"
-        password = "1234"  # Replace with the actual password
-
-        user = User.objects.get(username=username)
-
-        return JsonResponse({
-            "exists": True,
-            "username": user.username,
-            "is_active": user.is_active,
-            "check_password": user.check_password(password),
-            "authenticate": authenticate(
-                username=username,
-                password=password
-            ) is not None,
-        })
-
-    except Exception as e:
-        return JsonResponse({
-            "error": str(e),
-            "type": type(e).__name__,
-        }, status=500)
+    return JsonResponse({
+        "db_name": settings.DATABASES["default"]["NAME"],
+        "db_host": settings.DATABASES["default"]["HOST"],
+        "users": list(User.objects.values("id", "username", "email")),
+    })
